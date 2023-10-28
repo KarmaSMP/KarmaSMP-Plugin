24568b4d431a2eeee136e57292b845c42b52dc41
MrKacafirekCZ <mrkacafirekcz@gmail.com>
2023-10-28 15:21:10 +0200

    Added ability to control life system using a command

--- src/main/java/io/github/karmasmp/karmaplugin/command/LivesCommand.java
+++ src/main/java/io/github/karmasmp/karmaplugin/command/LivesCommand.java
@@ -16,7 +16,7 @@ import org.bukkit.entity.Player;
 public class LivesCommand {
 
     public enum LivesCommandActions {
-        ADD, REMOVE, SET, VIEW
+        ACTIVATE, ADD, DEACTIVATE, REMOVE, SET, VIEW
     }
 
     @CommandMethod("lives <player> <action> [value]")
@@ -29,19 +29,50 @@ public class LivesCommand {
         KarmaPlayer karmaPlayer = lifecycle.getKarmaPlayer(player);
 
         switch (action) {
+            case ACTIVATE -> activateLives(karmaPlayer);
             case ADD -> addLives(karmaPlayer, value);
+            case DEACTIVATE -> deactivateLives(karmaPlayer);
             case REMOVE -> removeLives(karmaPlayer, value);
             case SET -> setLives(karmaPlayer, value);
             case VIEW -> viewLives(karmaPlayer);
         }
     }
 
+    private void activateLives(KarmaPlayer karmaPlayer) {
+        setActiveLives(karmaPlayer, true);
+    }
+
     private void addLives(KarmaPlayer karmaPlayer, int lives) {
         setLives(karmaPlayer, karmaPlayer.getLives() + lives);
     }
 
+    private void deactivateLives(KarmaPlayer karmaPlayer) {
+        setActiveLives(karmaPlayer, false);
+    }
+
+    private Component getActiveLivesComponent(KarmaPlayer karmaPlayer) {
+        if (karmaPlayer.isActiveLives()) {
+            return Component.text("activated lives", NamedTextColor.GREEN);
+        }
+
+        return Component.text("deactivated lives", NamedTextColor.RED);
+    }
+
     private void removeLives(KarmaPlayer karmaPlayer, int lives) {
         setLives(karmaPlayer, karmaPlayer.getLives() - lives);
+    }
+
+    private void setActiveLives(KarmaPlayer karmaPlayer, boolean activeLives) {
+        karmaPlayer.setActiveLives(activeLives);
+        karmaPlayer.sendMessage(Component.empty()
+                .append(Component.text("Player", NamedTextColor.YELLOW))
+                .appendSpace()
+                .append(Component.text(karmaPlayer.getName(), NamedTextColor.GOLD))
+                .appendSpace()
+                .append(Component.text("now has", NamedTextColor.YELLOW))
+                .appendSpace()
+                .append(getActiveLivesComponent(karmaPlayer))
+                .append(Component.text("!", NamedTextColor.YELLOW)));
     }
 
     private void setLives(KarmaPlayer karmaPlayer, int lives) {
@@ -66,6 +97,15 @@ public class LivesCommand {
                 .append(Component.text("has", NamedTextColor.YELLOW))
                 .appendSpace()
                 .append(Component.text(karmaPlayer.getLives() + " lives", NamedTextColor.GREEN))
+                .append(Component.text("!", NamedTextColor.YELLOW))
+                .appendNewline()
+                .append(Component.text("Player", NamedTextColor.YELLOW))
+                .appendSpace()
+                .append(Component.text(karmaPlayer.getName(), NamedTextColor.GOLD))
+                .appendSpace()
+                .append(Component.text("also has", NamedTextColor.YELLOW))
+                .appendSpace()
+                .append(getActiveLivesComponent(karmaPlayer))
                 .append(Component.text("!", NamedTextColor.YELLOW)));
     }
 }
