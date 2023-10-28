dc36e9af8774533b37236c1fe9545582b48fe2eb
MrKacafirekCZ <mrkacafirekcz@gmail.com>
2023-10-28 15:03:50 +0200

    Lives now decrease upon death

--- src/main/java/io/github/karmasmp/karmaplugin/phase/world/DefaultWorldPhase.java
+++ src/main/java/io/github/karmasmp/karmaplugin/phase/world/DefaultWorldPhase.java
@@ -1,10 +1,20 @@
 package io.github.karmasmp.karmaplugin.phase.world;
 
+import io.github.karmasmp.karmaplugin.KarmaPlayer;
+import io.github.karmasmp.karmaplugin.event.player.KarmaPlayerDeathEvent;
 import io.github.karmasmp.karmaplugin.lifecycle.world.WorldLifecycle;
 
 public class DefaultWorldPhase extends WorldPhase {
 
     public DefaultWorldPhase(WorldLifecycle lifecycle) {
         super(lifecycle, Integer.MAX_VALUE);
+    }
+
+    @Override
+    public boolean event(KarmaPlayerDeathEvent event) {
+        KarmaPlayer karmaPlayer = event.getKarmaPlayer();
+
+        karmaPlayer.setLives(karmaPlayer.getLives() - 1);
+        return true;
     }
 }
